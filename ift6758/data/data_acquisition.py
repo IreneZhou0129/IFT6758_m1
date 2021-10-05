84d709cc6aecf195c2392f0375e113bcdb05db16
IreneZhou0129 <xiaoxinzhou0@gmail.com>
2021-10-05 10:11:51 -0400

    add get_playoff_number, enhance get_data, which is able to download regular season data now

--- ift6758/data/data_acquisition.py
+++ ift6758/data/data_acquisition.py
@@ -3,9 +3,95 @@ Task 2:
 Create a function or class to download NHL play-by-play data
  for both the regular season and playoffs.
 """
-
+import os
 import re
+import json
+import time
+import numpy as np
 import requests
+
+def get_playoff_number():
+    """
+    Get all available playoff numbers. Notice that since 1st-digit is always 0, 
+    this function only returns the last three digits. General naming rules:
+        - 1st-digit: only 0
+        - 2nd-digit in [1, 2, 3, 4] 
+        - 3rd-digit bases on the 2nd:
+            * if 2nd == 1: 3rd in [1, ..., 8]
+            * elif 2nd == 2: 3rd in [1, ..., 4]
+            * elif 2nd == 3: 3rd in [1, 2]
+            * elif 2nd == 4: 3rd in [1]
+        - 4st-digit in [1, ..., 7]
+
+    Parameters:
+        None
+
+    Returns:
+        playoff_numbers_dict(list): A dict of lists of 3-digit strings. 
+                                    The first level of keys is the round number, or the 2nd-digit.
+    """
+    print("running get_playoff_number()")
+
+    third_digit = fourth_digit = 0
+
+    playoff_number_str = ""
+    playoff_numbers_list = playoff_numbers_matrix = []
+    playoff_numbers_dict = {
+        "2nd-digit": {
+            "1":[],
+            "2":[],
+            "3":[],
+            "4":[]
+        }
+    }
+
+    # Idea: ignore the first digit '0', create integers and then cast them to strings. 
+    third_digit_range = [8, 4, 2, 1]
+
+    # i+1 from 1 to 4, represents the 2nd-digit's four values: 1, 2, 3, and 4. 
+    for i in range(len(third_digit_range)):
+
+        current_limit = third_digit_range[i]
+
+        playoff_numbers_list = []
+
+        # 3rd and 4th digit always starts from 1
+        start_number = int(str(i+1)+'11')
+
+        # calculate the 3rd-digit range because it depends on the 2nd-digit
+        end_number = start_number + current_limit*10
+        
+        for j in range(start_number, end_number, 10):
+
+            # 4th-digit is independent, it always has values from 1 to 7
+            current_list = list(np.arange(j, j+7, 1)) 
+            playoff_numbers_list.append(current_list)
+
+        # convert the list of lists to a Numpy array
+        playoff_numbers_matrix = np.array(playoff_numbers_list)
+
+        playoff_numbers_dict["2nd-digit"][str(i+1)] = playoff_numbers_matrix
+        
+        return playoff_numbers_dict
+        # TODO: for the purpose of discussion only, need to remove it before submit to main
+        # > playoff_numbers_dict
+        # {'2nd-digit': 
+        #     {'1': array([[111, 112, 113, 114, 115, 116, 117],
+        #             [121, 122, 123, 124, 125, 126, 127],
+        #             [131, 132, 133, 134, 135, 136, 137],
+        #             [141, 142, 143, 144, 145, 146, 147],
+        #             [151, 152, 153, 154, 155, 156, 157],
+        #             [161, 162, 163, 164, 165, 166, 167],
+        #             [171, 172, 173, 174, 175, 176, 177],
+        #             [181, 182, 183, 184, 185, 186, 187]]),
+        #     '2': array([[211, 212, 213, 214, 215, 216, 217],
+        #             [221, 222, 223, 224, 225, 226, 227],
+        #             [231, 232, 233, 234, 235, 236, 237],
+        #             [241, 242, 243, 244, 245, 246, 247]]),
+        #     '3': array([[311, 312, 313, 314, 315, 316, 317],
+        #             [321, 322, 323, 324, 325, 326, 327]]),
+        #     '4': array([[411, 412, 413, 414, 415, 416, 417]])}}
+
 
 def generate_game_id(season, game_type, game_number):
     """
@@ -19,7 +105,12 @@ def generate_game_id(season, game_type, game_number):
         game_number(str): A 4-digit string.
             - For regular season and preseason games, this ranges from 0001 to the number of games played. 
               (1271 for seasons with 31 teams (2017 and onwards) and 1230 for seasons with 30 teams). 
-              31 * 82 / 2 = 1271
+              
+              2016-2020 (2016-17 ~ 2020-21 season)
+              2016: 30 * 82 / 2 = 1230 <---- max
+              between 2017 to 2020: 31 * 82 / 2 = 1271 <---- max
+              NOTE: loop over from 1 to max depends on the year 
+
             - For playoff games, the 2nd digit of the specific number gives the round of the playoffs, 
               the 3rd digit specifies the matchup, and the 4th digit specifies the game (out of 7).
 
@@ -28,51 +119,101 @@ def generate_game_id(season, game_type, game_number):
 
     Reference: https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md#game-ids
     """
-    print("running generate_game_id()")
-
     game_id = f"{season}{game_type}{game_number}"
 
     # checks if the game id is a 10-digit string.
     game_id_pattern = re.compile(r"[0-9]{10}")
 
     if bool(game_id_pattern.match(game_id)):
-        print(f"Game ID is: {game_id}.")
         return game_id
 
     else:
-        print(f"Please check the inputs formats.")
-        return None
-
+        print(f"Please check the inputs formats.\n \
+                Current: \n\
+                season - {season}\n \
+                game_type - {game_type}\n \
+                game_number - {game_number}")
+        raise Exception
 
 def get_data():
     """
     Download data from the 2016-17 season all the way up to the 2020-21 season.
     """
-    # TODO: number of teams
-    teams = None
+    t0=time.time()
     game_id = ""
 
     # save JSON outputs locally
-    # TODO: add the destination folder to .gitignore
-    data_local_path = ""
+    data_local_path = "/Users/xiaoxinzhou/Documents/IFT6758_JSON_data"
 
-    seasons = ["2016", "2017", "2018", "2019", "2020"]
+    years = ["2016", "2017", "2018", "2019", "2020"]
 
     # game types
     regular_season = "02"
     playoffs = "03"
 
-    # game numbers
-    regular_games = teams*82/2
+    for year in years:
+        # As the API mentions, there are 30 teams in 2016
+        # and 31 teams in 2017, 2018, 2019, and 2020 each.
+        teams = 30 if year == "2016" else 31
+        
+        # how many games happened
+        regular_games = teams*82//2
+
+        # ============================
+        # regular season
+        # ============================
+        regular_data_path = f"{data_local_path}/regular_season/{year}"
+
+        for game in range(regular_games):
+
+            # convert a number to a 4-digit string: 5 -> "0005", 123 -> "0123"
+            game_number = str(game).zfill(4)
+
+            game_id = generate_game_id(year, regular_season, game_number)
+            
+            response = requests.get(f"https://statsapi.web.nhl.com/api/v1/game/{game_id}/feed/live/")
+            data = response.json()
+
+            # skip the empty game info
+            if "message" in data and data["message"] == "Game data couldn't be found":
+                continue
+
+            # create file path if it doesn't exist
+            filename = f"{regular_data_path}/{game_id}.json"
+            dirname = os.path.dirname(filename)
+            if not os.path.exists(dirname):
+                os.makedirs(dirname)
+
+            # write data to local
+            with open(filename, 'w+') as f:
+                json.dump(data, f, sort_keys=True, indent=4)
+        
+    t1=time.time()
+    print(f"total time: {t1-t0}") 
+    # total time: 1239.0541338920593
+    # >>> import datetime
+    # >>> str(datetime.timedelta(seconds=1239))
+    # '0:20:39'
+        # ============================
+        # playoffs
+        # ============================ 
+        # playoffs_data_path = data_local_path+"/playoff_season"       
+
+
 
     # response = requests.get(f"https://statsapi.web.nhl.com/api/v1/game/{game_id}/feed/live/")
-    response = requests.get(f"https://statsapi.web.nhl.com/api/v1/game/2016020001/feed/live/")
+    # response = requests.get(f"https://statsapi.web.nhl.com/api/v1/game/2016020001/feed/live/")
 
-    data = response.json()    
+    # data = response.json()    
 
-    # TODO: save outputs by year? type of game? 
+    # TODO: save outputs by year? type of game? compare playoffs and regular JSON formats and decide how to organize these
+    
 
-    breakpoint()
+    # TODO: skip the empty JSON 
+    # regular_season = [JSON_dest]
+    # playoffs_season = [JSON_dest]
+
 
 # generate_game_id("2017", "02", "1234")
+# get_playoff_number()
 get_data()
