d72296e6f8700dff68613ffdb31906f17a03ac22
IreneZhou0129 <xiaoxinzhou0@gmail.com>
2021-11-30 11:30:57 -0500

    finished part 5 q3

--- ift6758/data/milestone2/q5.py
+++ ift6758/data/milestone2/q5.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from sklearn import preprocessing
 
 # xgboost
 import os
@@ -43,6 +44,9 @@ from sklearn.ensemble import ExtraTreesClassifier
 # need to install graohviz
 # > conda install graphviz python-graphviz
 
+# ##################################################
+# Train XGBoost using only 'distance' and 'angle'
+# ##################################################
 def train(X, y, features=['Distance from Net']):
     '''
     reference: https://machinelearningmastery.com/develop-first-xgboost-model-python-scikit-learn/
@@ -95,17 +99,10 @@ def q5_1_plots():
     X,y = read_dataset()
     plot_models(X, y, 'xgb')
 
-def q5_2():
-    
-    # Read CSV files
-    dataset = pd.read_csv('/Users/xiaoxinzhou/Documents/IFT6758_M2_CSV_data/all_data_categorical.csv')
-
-    # Separate features and labels 
-    X = dataset[['eventIdx', 'game_id', 'Game Seconds', 'Game Period', 'X-Coordinate', 'Y-Coordinate',
-               'Shot Distance', 'Shot Angle', 'Shot Type', 'Was Net Empty', 'Last Event Type', 'Last X-Coordinate',
-               'Last Y-Coordinate', 'Time from Last Event (seconds)', 'Distance from Last Event', 'Is Rebound',
-               'Change in Shot Angle', 'Speed']]
-    y = dataset[['Is Goal']]
+# ##################################################
+# Train XGBoost using all features
+# ##################################################
+def q5_2(X, y):
 
     # initialize comet.ml experiment
     experiment = Experiment(
@@ -175,7 +172,12 @@ def q5_2():
     # return X_test_pred_proba, y_test, clf, X_test
     
 
-def q5_3(X, y):
+# ###################################################
+# Feature selection
+# ref: 
+# - https://scikit-learn.org/stable/modules/feature_selection.html
+# ###################################################
+def q5_3(X, y, experiment):
 
     # Create a training and validation split
     X_train, X_test, y_train, y_test = train_test_split(X,
@@ -194,132 +196,127 @@ def q5_3(X, y):
     # Evaluate predictions
     accuracy = metrics.accuracy_score(y_test, y_test_pred)
     print("Accuracy: %.2f%%" % (accuracy * 100.0))
+
+    # selected_features = list(X.columns)
+
+    params = {
+        'model_type': 'xgboost',
+        # 'selected_features': selected_features
+    }
+
+    metrics_dict = {
+        'accuracy': accuracy
+    }
     
+    experiment.log_parameters(params)
+    experiment.log_metrics(metrics_dict)
+
     return model
     
 
-def q5_3_var_threshold(X, y):
-    experiment = Experiment(
-        api_key = os.environ.get("COMET_API_KEY"),
-        project_name = 'milestone_2',
-        workspace='xiaoxin-zhou')   
-
-    print(X.shape)
+# https://scikit-learn.org/stable/modules/feature_selection.html#removing-features-with-low-variance
+def q5_3_var_threshold(X, y, experiment):  
 
     sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
     sel_reduced = sel.fit_transform(X)
-    print(sel_reduced.shape)
     
-    model = q5_3(sel_reduced, y)
+    model = q5_3(sel_reduced, y, experiment)
     
     experiment.log_dataset_hash(sel_reduced)
     
     # https://github.com/comet-ml/comet-examples/blob/master/model_registry/xgboost_seldon_aws/xgboost_seldon_aws.ipynb
-    os.makedirs("output", exist_ok=True)
-    model.save_model("output/q5_3_var_threshold.model")
+    # os.makedirs("output", exist_ok=True)
+    model.save_model("models/q5_3_var_threshold.model")
     model_name = "XGBoost Model (var threshold)"
-    experiment.log_model(model_name, "output/q5_3_var_threshold.model")
+    experiment.log_model(model_name, "models/q5_3_var_threshold.model")
     experiment.end()
-
-    # experiment.log_dataset_hash(sel_reduced)
     
     # return sel_reduced.shape, sel_reduced
 
 
-def q5_3_selectKbest(X, y):
-    experiment = Experiment(
-        api_key = os.environ.get("COMET_API_KEY"),
-        project_name = 'milestone_2',
-        workspace='xiaoxin-zhou')    
-
-    # print(X)
+# https://scikit-learn.org/stable/modules/feature_selection.html#univariate-feature-selection
+def q5_3_selectKbest(X, y, experiment):  
+    
     min_max_scaler = preprocessing.MinMaxScaler()
     for col in X:
         if X[col].dtypes != np.object:
             # https://www.kite.com/python/answers/how-to-scale-pandas-dataframe-columns-with-the-scikit-learn-minmaxscaler-in-python
             X[[col]] = min_max_scaler.fit_transform(X[[col]])
 
-    # print(X)
-    # print(X_normalized)
-
     X_new = SelectKBest(chi2, k=6).fit_transform(X, y)
     
-    model = q5_3(X_new, y)
+    model = q5_3(X_new, y, experiment)
     
     experiment.log_dataset_hash(X_new)
     
-#     model = q5_3(sel_reduced, y)
-    
-#     experiment.log_dataset_hash(sel_reduced)
-    
     # https://github.com/comet-ml/comet-examples/blob/master/model_registry/xgboost_seldon_aws/xgboost_seldon_aws.ipynb
-    os.makedirs("output", exist_ok=True)
-    model.save_model("output/q5_3_selectKBest.model")
+    # os.makedirs("output", exist_ok=True)
+    model.save_model("models/q5_3_selectKBest.model")
     model_name = "XGBoost Model (q5_3_selectKbest)"
-    experiment.log_model(model_name, "output/q5_3_selectKBest.model")
+    experiment.log_model(model_name, "models/q5_3_selectKBest.model")
     experiment.end()
         
     # return X_new.shape, X_new
 
 
-def q5_3_selectFromModel():
-    experiment = Experiment(
-        api_key = os.environ.get("COMET_API_KEY"),
-        project_name = 'milestone_2',
-        workspace='xiaoxin-zhou') 
+# https://scikit-learn.org/stable/modules/feature_selection.html#feature-selection-using-selectfrommodel
+def q5_3_selectFromModel(X, y, experiment):
 
     lsvc = LinearSVC(C=0.01, penalty="l1", dual=False).fit(X, y)
     model = SelectFromModel(lsvc, prefit=True)
     X_new = model.transform(X)
-    X_new.shape
     
-    model = q5_3(X_new, y)
+    model = q5_3(X_new, y, experiment)
     
     experiment.log_dataset_hash(X_new)
     
     # https://github.com/comet-ml/comet-examples/blob/master/model_registry/xgboost_seldon_aws/xgboost_seldon_aws.ipynb
-    os.makedirs("output", exist_ok=True)
-    model.save_model("output/q5_3_selectFromModel.model")
+    # os.makedirs("output", exist_ok=True)
+    model.save_model("models/q5_3_selectFromModel.model")
     model_name = "XGBoost Model (selectFromModel)"
-    experiment.log_model(model_name, "output/q5_3_selectFromModel.model")
+    experiment.log_model(model_name, "models/q5_3_selectFromModel.model")
     experiment.end()
 
     # return X_new
 
 
-def q5_3_extraTree():
-    experiment = Experiment(
-        api_key = os.environ.get("COMET_API_KEY"),
-        project_name = 'milestone_2',
-        workspace='xiaoxin-zhou') 
-    
-    # Read CSV files
-    # dataset = pd.read_csv('/Users/sunjiaao/Courses/IFT6758/m2_CSV_data/all_data_q4_categorical.csv')
+def q5_3_extraTree(X, y, experiment): 
 
     clf = ExtraTreesClassifier(n_estimators=50)
     clf = clf.fit(X, y)
-    print(clf.feature_importances_)
+    # print(clf.feature_importances_)
 
     model = SelectFromModel(clf, prefit=True)
     X_new = model.transform(X)
-    print(X_new.shape)
     
-    model = q5_3(X_new, y)
+    model = q5_3(X_new, y, experiment)
     
     experiment.log_dataset_hash(X_new)
     
     # https://github.com/comet-ml/comet-examples/blob/master/model_registry/xgboost_seldon_aws/xgboost_seldon_aws.ipynb
-    os.makedirs("output", exist_ok=True)
-    model.save_model("output/q5_3_extraTree.model")
+    # os.makedirs("output", exist_ok=True)
+    model.save_model("models/q5_3_extraTree.model")
     model_name = "XGBoost Model (extraTree)"
-    experiment.log_model(model_name, "output/q5_3_extraTree.model")
+    experiment.log_model(model_name, "models/q5_3_extraTree.model")
     experiment.end()
     # return X_new.shape, X_new
 
 
 if __name__ == '__main__':
-    # X,y = read_dataset()
-    # train(X, y, features=['Distance from Net','Angle from Net'])
+    
+    X,y = read_all_features()
+
+    experiment = Experiment(
+        api_key = os.environ.get("COMET_API_KEY"),
+        project_name = 'milestone_2',
+        workspace='xiaoxin-zhou') 
+
+    # q5_3_var_threshold(X, y, experiment)    
+    # q5_3_selectKbest(X, y, experiment)
+    # q5_3_selectFromModel(X, y, experiment)
+    q5_3_extraTree(X, y, experiment)
+
+    # train(X, y)
     
     # q5_1_plots()
 
